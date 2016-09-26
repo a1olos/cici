ba736bf58952aa6d1f9d27da13ceafb6e0a6e06b
a1olos <292673791@qq.com>
2016-09-26 14:49:53 +0800

    TestGitCommit

---
+++ src/test/java/com/cn/test/TestGitCommit.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:cici
+ * File Name:TestGitCommit.java
+ * Package Name:com.cn.test
+ * Date:2016年9月26日下午2:49:15
+ * Copyright (c) 2016, All Rights Reserved.
+ *
+*/
+
+package com.cn.test;
+/**
+ * ClassName:TestGitCommit <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2016年9月26日 下午2:49:15 <br/>
+ * @author   TH
+ * @version  
+ * @since    JDK 1.7
+ * @see 	 
+ */
+public class TestGitCommit {
+
+}
+

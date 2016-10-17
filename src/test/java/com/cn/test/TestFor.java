ee6dbc036d899163b02973ef89f2c69a710a0045
a1olos <292673791@qq.com>
2016-10-17 10:20:58 +0800

    test for

---
+++ src/test/java/com/cn/test/TestFor.java
@@ -0,0 +1,32 @@
+/**
+ * Project Name:cici
+ * File Name:TestFor.java
+ * Package Name:com.cn.test
+ * Date:2016年10月14日上午11:07:07
+ * Copyright (c) 2016, All Rights Reserved.
+ *
+*/
+
+package com.cn.test;
+/**
+ * ClassName:TestFor <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2016年10月14日 上午11:07:07 <br/>
+ * @author   TH
+ * @version  
+ * @since    JDK 1.7
+ * @see 	 
+ */
+public class TestFor {
+
+	public static void main(String[] args) {
+		for(int i = 1 ;i>0; i++){
+			System.out.println(i);
+			if(i == 10){
+				break;
+			}
+		}
+	}
+}
+
